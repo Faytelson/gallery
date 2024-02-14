7c7230f96712d0fa218e4be91c9e7425a8315c49
Faytelson <dfaitelson@scid.ru>
2024-02-14 17:25:20 +0300

    button-scroll up

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,9 @@
       <div class="app__preloader">
         <Preloader></Preloader>
       </div>
+      <div class="app__button-scroll-top" v-show="isButtonScrollTop">
+        <ButtonMain styleType="scroll-top" @clickBtn="scrollToTop"></ButtonMain>
+      </div>
     </v-app>
   </div>
 </template>
@@ -19,20 +22,47 @@
 <script>
 import NavBar from "@/components/NavBar.vue";
 import Preloader from "./components/Preloader.vue";
+import ButtonMain from "./components/ui/ButtonMain.vue";
 
 export default {
   name: "App",
   components: {
     NavBar,
     Preloader,
+    ButtonMain,
+  },
+  created() {
+    window.onscroll = () => {
+      this.checkScroll();
+    };
   },
   data() {
-    return {};
+    return {
+      isButtonScrollTop: false,
+    };
+  },
+  methods: {
+    checkScroll() {
+      this.isButtonScrollTop = document.documentElement.scrollTop >= 500;
+    },
+    scrollToTop() {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    },
   },
 };
 </script>
 
 <style lang="scss">
+.app {
+  &__button-scroll-top {
+    position: fixed;
+    bottom: 40px;
+    right: 20px;
+  }
+}
 .fade-enter {
   opacity: 0;
 }
---
+++ src/components/ui/ButtonMain.vue
@@ -0,0 +1,38 @@
+<template>
+  <button class="button" :class="getClasses" @click="emitClick"></button>
+</template>
+
+<script>
+export default {
+  name: "ButtonMain",
+  props: {
+    title: String,
+    styleType: String,
+  },
+  computed: {
+    getClasses() {
+      let classes = [];
+      if (this.styleType) {
+        classes.push(`button_${this.styleType}`);
+      }
+      return classes;
+    },
+  },
+  methods: {
+    emitClick() {
+      this.$emit("clickBtn");
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.button {
+  &_scroll-top {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    background: url("@/assets/images/icon_scroll-top.webp") center/contain no-repeat;
+  }
+}
+</style>
